cec68deb39fc5b80407bf5661f7eabb42a75d880
JohnDavid Abe <johndavidabe101@gmail.com>
2025-09-15 17:50:39 -0400

    DS and Vector start

---
+++ DS/vector.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+
+// Vector implementation
+class Vector {
+
+    int* data;
+
+    Vector() {
+        int data[100] = {0};
+    }
+
+    Vector(int capacity) {
+        int data[capacity] = {};
+    }
+
+};
+
+
+
+int main() {
+    Vector test();
+}
